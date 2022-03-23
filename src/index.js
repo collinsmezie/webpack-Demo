619e997961cca90ab9abc67c7377f8067999db98
collinsmezie <collinschimezieobinwa@gmail.com>
2022-03-23 14:55:35 +0400

    completed webpack course

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import _ from 'lodash';
+import './style.css';
+
 
 
 function component() {
@@ -6,6 +8,8 @@ function component() {
   
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
   
     return element;
   }
